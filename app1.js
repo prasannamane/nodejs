2879897e855e69b6a600e285b15f86c83d06f7aa
“Prasanna <prasanna.mane@bajajfinserv.in>
2023-01-02 15:42:57 +0530

    test

--- app1.js
+++
@@ -1,8 +0,0 @@
-
-function one(one){
-    console.log(one);
-    return one;
-}
-
-const one = one(1);
-console.log(one);
--- routes/SetupRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const Setup = require('../controllers/SetupController');
-const router = express.Router();
-
-router.route('/').get(Setup.connection);
-module.exports = router
