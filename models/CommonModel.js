d46c565c26d77cddbcdb0a4070d13cd66344b2e8
“Prasanna <prasanna.mane@bajajfinserv.in>
2023-01-03 10:04:09 +0530

    Update

--- models/CommonModel.js
+++ models/CommonModel.js
@@ -5,19 +5,26 @@ class CommonModel {
         mongoose.connect('mongodb://127.0.0.1:27017/subscriber')
             .then(() => console.log('Connected to MongoDB...'))
             .catch(err => console.error('Could not connect to MongoDB...', err));
-            const subscribeSchema = new mongoose.Schema({
-                name: String,
-                email: String,
-            });
-            return mongoose.model('Subscribe', subscribeSchema);
+        const subscribeSchema = new mongoose.Schema({
+            name: {
+                type: String, 
+                required: true,
+                minlenght: 5,
+                maxlenght: 20
+            },
+            date: { type: Date, default: Date.now },
+            email: String,
+            
+        });
+        return mongoose.model('Subscribe', subscribeSchema);
     }
-   
+
     creteSubscribe(Subscribe) {
         return new Subscribe({
             name: 'Subscribe Chennel Now',
             email: 'prasannamane7@gmail.com',
             author: 'Prasanna',
-            tags: ['node', 'backend'], 
+            tags: ['node', 'backend'],
             isPublished: true
         });
     }
